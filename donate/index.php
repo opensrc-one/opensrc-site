2687564a183cc2942bbbe46a4c10a851d9b2e036
Ash Schmüidt <47500413+Ashintosh@users.noreply.github.com>
2023-02-28 13:06:27 -0800

    Removed XSS vulnerability from donation page; Reduced image size of QR codes
    - Fixed bug allowing XSS scripting on donation page
    - Reduced image size of QR codes on donation page

--- donate/index.php
+++ donate/index.php
@@ -1,6 +1,14 @@
 <?php
-if (!isset($_GET['ticker']) || !isset($_GET['name']) || !isset($_GET['address']) || !isset($_GET['qr'])) { header("Location: https://opensrc.one/"); exit; }
-$selected_currency = array('ticker' => $_GET['ticker'], 'name' => $_GET['name'], 'address' => $_GET['address'], 'qr' => $_GET['qr']);
+if (!isset($_POST['view-address']) || !isset($_POST['coin'])) { header("Location: https://opensrc.one/"); exit; }
+
+$coin_ticker = $_POST['coin'];
+$coin_list = array (
+    'xmr' => array('name' => 'Monero', 'address' => '84W8YaFgeEXKoQz28QKZCq7T5UJGvjpQ4FEuTRdZrzknca9cXGhtf27gyvDTfQ9bvhUfQ22Pda5gDjUf9dE6CZEPTNKYAdq'),
+    'wow' => array('name' => 'Wownero', 'address' => 'WW3C6aNUEsY5YxnHYiZHh7dAihgURt72zd64tFLVnYfxDTyGpomQjji13xdNynLzC1cd5JauvXhCMKUVTHxQekyQ1PdCJF8Rs'),
+    'zec' => array('name' => 'ZCash', 'address' => 'zs128cux9cuc8cj0739a4m0usp706vtukxsvvagmk0qtuetr9w45x66hr0ftms4cnyg46s765r3fev'),
+    'ltc' => array('name' => 'Litecoin', 'address' => 'ltc1q6h8rdjan4hjh8yfnrdyu6cmpzt9klyqkyy58qv'),
+    'btc' => array('name' => 'Bitcoin', 'address' => 'bc1q6zwj347tu804qg3486ee86r7cwdt4z82hq4tlp')
+);
 ?>
 
 <!DOCTYPE html>
@@ -14,7 +22,7 @@ $selected_currency = array('ticker' => $_GET['ticker'], 'name' => $_GET['name'],
         <meta name="author" content="opensrc">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<meta http-equiv="Onion-Location" content="http://opensrc5aqxbfanpzgyrn2tjq7juvpuvqm5rrw2ztxcxooajpsjyfrid.onion">
+        <meta http-equiv="Onion-Location" content="http://opensrc5aqxbfanpzgyrn2tjq7juvpuvqm5rrw2ztxcxooajpsjyfrid.onion">
         <link rel="icon" type="image/x-icon" href="../static/img/favicon.ico">
 
         <!-- Styles -->
@@ -42,7 +50,7 @@ $selected_currency = array('ticker' => $_GET['ticker'], 'name' => $_GET['name'],
             <div class="page">
                 <div class="home">
                     <div class="page-header">
-                        <h2><?php echo($selected_currency['name'] ." (". $selected_currency['ticker'] .")"); ?> Donation Address</h2>
+                        <h2><?php echo($coin_list[$selected_coin]['name'] . ' (' . strtoupper($coin_ticker) . ')'); ?> Donation Address</h2>
                     </div>
                     <div class="page-content">
                         <p>
@@ -53,10 +61,10 @@ $selected_currency = array('ticker' => $_GET['ticker'], 'name' => $_GET['name'],
                 <hr>
                 <div class="center-container">
                     <div class="crypto-address">
-                        <textarea name="" id="" cols="80" rows="8" disabled><?php echo($selected_currency['address']); ?></textarea>
+                        <textarea name="" id="" cols="80" rows="8" disabled><?php echo($coin_list[$coin_ticker]['address']); ?></textarea>
                     </div>
                     <div class="qr-code">
-                        <img src="../static/img/crypto-qr/<?php echo($selected_currency['qr']); ?>" alt="QR Code">
+                        <img src="../static/img/crypto-qr/<?php echo($coin_ticker . '-qr.png'); ?>" alt="QR Code">
                     </div>
                 </div>
             </div>
