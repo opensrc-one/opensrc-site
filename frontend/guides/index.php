81c7aa0135dc84dfd250c7a0715b43d9f1c1b4d5
Ash Schmüidt <47500413+Ashintosh@users.noreply.github.com>
2023-03-01 02:21:46 -0800

    Restructure source for backend implementation

---
+++ frontend/guides/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>opensrc.one - Guides</title>
+
+    <meta charset="UTF-8">
+    <meta name="description" content="Open source software and services.">
+    <meta name="keywords" content="opensrc, monero, anonymous, security, xmr, monero node, xmr node">
+    <meta name="author" content="opensrc">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Onion-Location" content="http://opensrc5aqxbfanpzgyrn2tjq7juvpuvqm5rrw2ztxcxooajpsjyfrid.onion/guides">
+    <link rel="icon" type="image/x-icon" href="../static/img/favicon.ico">
+
+    <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="../static/css/styles.css">
+
+    <!-- Fonts -->
+    <link rel="stylesheet" type="text/css" href="../static/css/fonts.css">
+</head>
+<body>
+<div class="center-container">
+    <div class="header">
+        <div class="logo">
+            <img src="../static/img/logo.png" alt="opensrc.one">
+        </div>
+        <div class="nav">
+            <ul>
+                <li><a href="../..">Home</a></li>
+                <li><a href="../xmrnode">XMR Node</a></li>
+                <li><a class="active">Guides</a></li>
+                <li><a href="../contact">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="content">
+    <div class="page">
+        <div class="home">
+            <div class="page-header">
+                <h2>Guides</h2>
+            </div>
+            <div class="page-content">
+                <p>
+                    Guides and information on tools and services that help you protect your online privacy.
+                </p>
+            </div>
+        </div>
+        <hr>
+        <div class="node-list">
+            <div class="page-content">
+                <ul>
+                    <li>
+                        <span><h2>Guide 1 - Test One</h2></span>
+                        <p>This is a test description for the first guide.</p>
+                    </li>
+                    <li>
+                        <span><h2>Guide 2 - Test Two</h2></span>
+                        <p>This is a test description for the second guide.</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="footer">
+    <ul>
+        <li><a href="../legal/privacy-policy" target="_blank">Privacy Policy</a></li>
+        <li><a href="https://github.com/Ashintosh/opensrc.one" target="_blank">Source</a></li>
+        <li><a href="../pgp.txt" target="_blank">PGP</a></li>
+        <li><a href="http://opensrc5aqxbfanpzgyrn2tjq7juvpuvqm5rrw2ztxcxooajpsjyfrid.onion/xmrnode">Onion</a></li>
+        <li><a href="../canary.txt" target="_blank">Canary</a></li>
+    </ul>
+</div>
+</body>
+</html>
